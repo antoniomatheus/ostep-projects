84917644f6ef9c77642369737ee60f40dc04dda3
Antonio Matheus <antonimathsilva@gmail.com>
2020-04-12 09:59:48 -0300

    wcat done

---
+++ initial-utilities/wcat/wcat.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUFSIZE 200
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    return 0;
+  }
+
+  char *buf = malloc(BUFSIZE); 
+  if (buf == NULL) {
+    fprintf(stderr, "wcat: failed to allocate memory\n");
+    exit(1);
+  }
+
+  for (int i = 1; i < argc; i++) {
+    FILE *fp = fopen(argv[i], "r");
+
+    if (fp == NULL) {
+      printf("wcat: cannot open file\n");
+      exit(1);
+    }
+
+    while (fgets(buf, BUFSIZE, fp) != NULL) {
+      printf("%s", buf);
+    }
+  }
+
+  return 0;
+}
