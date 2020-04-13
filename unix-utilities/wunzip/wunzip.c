0fde214ca6d782b72c62d9bf529f4f4ea2790714
Antonio Matheus <antonimathsilva@gmail.com>
2020-04-13 11:06:38 -0300

    wunzip done

---
+++ unix-utilities/wunzip/wunzip.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("wunzip: file1 [file2 ...]\n");
+    exit(1);
+  }
+
+  for (int i = 1; i < argc; i++) {
+    FILE *fs = fopen(argv[i], "r");
+
+    if (fs == NULL) {
+      printf("wzip: cannot open file\n");
+      exit(1);
+    }
+
+    int count;
+    char cur;
+
+    while (((int) fread(&count, sizeof (int), 1, fs)) > 0) {
+      fread(&cur, sizeof (char), 1, fs);
+
+      for (int j = 0; j < count; j++) {
+        printf("%c", cur);
+      }
+    }
+
+    fclose(fs);
+  }
+
+  return 0;
+}
